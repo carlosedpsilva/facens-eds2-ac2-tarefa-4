57aa8d830a4fb9f1afccbae1179eda437e96a045
vaaaarlos <35001712+vaaaarlos@users.noreply.github.com>
2023-04-05 22:10:13 -0300

    4th scenario

--- src/test/java/br/facens/eds2/AppTest.java
+++ src/test/java/br/facens/eds2/AppTest.java
@@ -62,4 +62,21 @@ public class AppTest {
         assertEquals(0, aluno.getCursosDisponiveisParaCompra());
     }
 
+    @Test
+    @DisplayName("DADO que o aluno escreveu 2 ou mais tópicos"
+            + "E não recebeu 5 ou mais curtidas em comentarios em topicos de outros alunos"
+            + "QUANDO o mês termina"
+            + "ENTÃO não ganha o direito de comprar mais um curso")
+    public void alunoEscreveu2OuMaisTopicosENaoRecebeu5OuMaisCurtidasEmComentariosQuandoMesTerminaEntaoEleNaoGanhaDireitoDeComprarMaisUmCurso() {
+        Aluno aluno = new Aluno();
+        aluno.setTopicosEscritos(2);
+        aluno.setCurtidasRecebidas(4);
+        aluno.setCursosComprados(0);
+        aluno.setCursosDisponiveisParaCompra(0);
+
+        aluno.verificarElegibilidadeParaCurso(CURSOS_DISPONIVEIS_NA_PLATAFORMA);
+
+        assertEquals(0, aluno.getCursosDisponiveisParaCompra());
+    }
+
 }
